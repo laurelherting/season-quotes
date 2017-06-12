9e5b0336abae69fa87f7fa7e741a0238dcb4f3b3
Evan Harmon <evan.harmon@levvel.io>
2017-06-12 09:05:33 -0700

    fixes by Evan

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import {
-  BrowseRouter as Router, 
-  Route, 
+  BrowseRouter as Router,
+  Route,
   Link
-} from 'react-router-dom'
-import './App.css';
+} from 'react-router-dom';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-     <section id="p">
-      <label>
-        input type="text" />
-      </label>
-     </section>
+      <div>
+        <section>
+          <p>These are tulips</p>
+        </section>
+        <section>
+          <p>These are paintbrushes</p>
+        </section>
+        <section>
+          <p>These are trees</p>
+        </section>
+      </div>
     );
   }
 }
 
-export default App; 
+export default App;
