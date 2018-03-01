098888080801ee337db38bb2bc736095b1b12a4f
Laurel Herting <laurel.herting@gmail.com>
2018-02-28 17:47:13 -0700

    add page text

--- src/app.js
+++ src/app.js
@@ -11,16 +11,16 @@ class App extends Component {
     return (
       <div>
         <section>
-          <p>spring text</p>
+          <p>"Though I do not believe that a plant will spring up where no seed has been, I have great faith in a seed... Convince me that you have a seed there, and I am prepared to expect wonders." --Henry David Thoreau</p>
         </section>
         <section>
-          <p>summer text</p>
+          <p>"Overhead hung a summer sky furrowed with the rush of rockets; and from the east a late moon, pushing up beyond the lofty bend of the coast, sent across the bay a shaft of brightness which paled to ashes in the red glitter of illuminated boats." --Edith Wharton, the House of Mirth</p>
         </section>
         <section>
-          <p>fall text</p>
+          <p>"Autumn is a second spring when every leaf is a flower." --Albert Camus</p>
         </section>
         <section>
-          <p>winter text</p>
+          <p>"In seed time learn, in harvest teach, in winter enjoy." --William Blake</p>
         </section>
       </div>
     );
